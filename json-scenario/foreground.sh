0d31922adae0b84806eb3973f6d1161741337abc
Simon <stauss.simon.01@gmail.com>
2022-03-11 20:35:11 +0100

    hide command input

--- json-scenario/foreground.sh
+++ json-scenario/foreground.sh
@@ -1,7 +1,9 @@
-#!/bin/bash +x
+#!/bin/bash
+stty -echo
 echo "Please wait while we're getting everything ready..."
 sleep 1; awaitInstall.sh
 echo "Preparation done!"
 echo "Logging into PostgreSQL"
 sudo -i -u postgres
 psql
+stty echo
