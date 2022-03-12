e1c6ad019f7e7eb0f9816ea6fd89dde6eca28640
Simon <stauss.simon.01@gmail.com>
2022-03-12 11:36:27 +0100

    permissions

--- json-scenario/background.sh
+++ json-scenario/background.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-chmod 777 grades_john.json
-chmod 777 grades_mary.json
-
 sudo apt update
 echo "done" >>/root/update-finished
 
 sudo apt install postgresql postgresql-contrib -y
 echo "done" >>/root/install-finished
+chown postgres /root
+
+chmod a+rw grades_john.json
+chmod a+rw grades_mary.json
